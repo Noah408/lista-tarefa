2e67fb983ba772dca8482ad42ec18ee2f8067622
Noah Esteves <noahesteves@gmail.com>
2023-12-23 18:33:17 +0000

    enfim

---
+++ src/app/component/btnperfil.tsx
@@ -0,0 +1,12 @@
+import { Bell } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function BtnPerfil() {
+    return(
+        <div className='h-fit w-40 flex justify-center gap-6'>
+            <button><Bell size={40} color="#000000" /></button>
+            <Link href={""} className="h-12 w-12 rounded-full overflow-hidden" ><Image src="/assets/noah.jpg" width={100} height={100} alt="" /></Link>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import Image from 'next/image'
 import VertMenu from './component/vertmenu'
+import BtnPerfil from './component/btnperfil'
 
 export default function Home() {
   return (
     <main className='bg-slate-200 h-screen w-screen flex'>
       <div className='h-full w-96 flex-col justify-between'>
-        <div className='w-full h-40'>
+        <div className='w-full h-36'>
 
         </div>
-        <div className='bg-[#5b7cfd] w-full h-[calc(100%-10rem)] radiustopright pt-40 pl-5'>
+        <div className='bg-[#5b7cfd] w-full h-[calc(100%-9rem)] radiustopright pt-36 pl-5'>
           <VertMenu/>
         </div>
       </div>
       <div className='h-full w-[calc(100%-24rem)] flex flex-col items-center'>
-        <div className='bg-black h-40 w-[90%]'>
-          
+        <div className='h-36 w-[90%] flex justify-between items-center'>
+          <span className='text-black text-5xl font-bold'>Home</span>
+          <BtnPerfil/>
         </div>
-        <div className='bg-red-600 h-[calc(95%-10rem)] w-[90%]'>
+        <div className='bg-red-600 h-[calc(95%-9rem)] w-[90%]'>
 
         </div>
       </div>
