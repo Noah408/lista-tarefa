78f9f37a9bd51ce2b00ec32445cb4283301efaa1
Noah Esteves <noahesteves@gmail.com>
2023-12-24 14:18:30 +0000

    links1

--- src/app/component/vertmenu.tsx
+++ src/app/component/vertmenu.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function VertMenu() {
     return(
         <div className=" h-fit w-full flex flex-col gap-4">
-            <Link  href={""} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><LayoutPanelLeft size={28} color="#ffffff" /> Home</Link>
-            <Link  href={""} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><GanttChartSquare size={28} color="#ffffff" /> Tarefas</Link>
-            <Link  href={""} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><BookUser size={28} color="#ffffff" /> Lista Telefonica</Link>
-            <Link  href={""} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><Settings size={28} color="#ffffff" /> Definições</Link>
+            <Link  href={"/"} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><LayoutPanelLeft size={28} color="#ffffff" /> Home</Link>
+            <Link  href={"/tarefas"} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><GanttChartSquare size={28} color="#ffffff" /> Tarefas</Link>
+            <Link  href={"/listatele"} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><BookUser size={28} color="#ffffff" /> Lista Telefonica</Link>
+            <Link  href={"/defini"} className="h-16 w-full focus:bg-[#819aff] rounded-l-full flex items-center pl-7 gap-7 text-2xl" ><Settings size={28} color="#ffffff" /> Definições</Link>
         </div>
     );
 }
