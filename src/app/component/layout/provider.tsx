dd32dde00ac08b90519392630f25083e23a94c72
Noah Esteves <noahesteves@gmail.com>
2023-12-24 15:09:02 +0000

    ajustes

---
+++ src/app/component/layout/provider.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Inter } from 'next/font/google'
+import BtnPerfil from '../btnperfil'
+import VertMenu from '../vertmenu'
+import { usePathname } from 'next/navigation'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function LayoutProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    const rota = usePathname()
+    function Page(){
+        if(rota === '/')return'Home'
+        if(rota === '/tarefas')return'Tarefas'
+        if(rota === '/defini')return'Definições'
+        if(rota === '/listatele')return'Lista Telefonica'
+        return null
+    } 
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <main className='bg-slate-200 h-screen w-screen flex'>
+      <div className='h-full w-96 flex-col justify-between'>
+        <div className='w-full h-36'>
+
+        </div>
+        <div className='bg-[#5b7cfd] w-full h-[calc(100%-9rem)] radiustopright pt-36 pl-5'>
+          <VertMenu/>
+        </div>
+      </div>
+      <div className='h-full w-[calc(100%-24rem)] flex flex-col items-center'>
+        <div className='h-36 w-[90%] flex justify-between items-center'>
+          <span className='text-black text-5xl font-bold'><Page/></span>
+          <BtnPerfil/>
+        </div>
+        <div className='bg-red-600 h-[calc(95%-9rem)] w-[90%]'>
+        {children}
+        </div>
+      </div>
+    </main>
+      </body>
+    </html>
+  )
+}
--- src/app/defini/page.tsx
+++ src/app/defini/page.tsx
@@ -3,24 +3,6 @@ import VertMenu from "../component/vertmenu";
 
 export default function Definicoes() {
     return(
-        <main className='bg-slate-200 h-screen w-screen flex'>
-      <div className='h-full w-96 flex-col justify-between'>
-        <div className='w-full h-36'>
-
-        </div>
-        <div className='bg-[#5b7cfd] w-full h-[calc(100%-9rem)] radiustopright pt-36 pl-5'>
-          <VertMenu/>
-        </div>
-      </div>
-      <div className='h-full w-[calc(100%-24rem)] flex flex-col items-center'>
-        <div className='h-36 w-[90%] flex justify-between items-center'>
-          <span className='text-black text-5xl font-bold'>Definições</span>
-          <BtnPerfil/>
-        </div>
-        <div className='bg-red-600 h-[calc(95%-9rem)] w-[90%]'>
-
-        </div>
-      </div>
-    </main>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, rem non id est magni vero assumenda iusto rerum maiores modi nobis labore minima mollitia accusamus. Minus expedita illo deleniti officia.</p>
     );
 }
