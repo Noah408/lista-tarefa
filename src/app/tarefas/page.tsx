9aa9cd02cf57d92b451cdc0689fb5e4be0d0ac19
Noah Esteves <noahesteves@gmail.com>
2023-12-26 13:42:56 +0000

    search

--- src/app/tarefas/page.tsx
+++ src/app/tarefas/page.tsx
@@ -1,8 +1,21 @@
-import BtnPerfil from "../component/btnperfil";
-import VertMenu from "../component/vertmenu";
+import { Search } from "lucide-react";
+
 
 export default function Tarefas() {
     return(
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, rem non id est magni vero assumenda iusto rerum maiores modi nobis labore minima mollitia accusamus. Minus expedita illo deleniti officia.</p>
+      <div className="w-full h-full flex flex-col gap-2">
+        <div className="w-full h-24 bg-black flex justify-between">
+          <div>
+
+          </div>
+          <div className="w-96 h-12 bg-white rounded-xl overflow-hidden flex items-center">
+            <button className="pl-5"><Search color="#6f6d6d" /></button>
+            <input type="text" name="" id="" placeholder="search" className="w-full h-full outline-none pl-4 text-[#6f6d6d]" />
+          </div>
+        </div>
+        <div className="w-full h-[] flex flex-col gap-2">
+
+        </div>
+      </div>
     );
 }
